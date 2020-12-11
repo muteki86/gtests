fcaecc636e661b541fe2567107109ea1b914008d
Chizco <franrulez@gmail.com>
2020-12-11 17:48:06 -0600

    buffer

--- 3dRenderer/3drenderer/src/main.c
+++ 3dRenderer/3drenderer/src/main.c
@@ -1,12 +1,18 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <SDL2/SDL.h>
 
 SDL_Renderer* renderer = NULL;
 SDL_Window* window = NULL;
-
 bool is_running = false;
+
+uint32_t* color_buffer = NULL;
+SDL_Texture* color_buffer_texture = NULL;
+
+int window_width = 800;
+int window_height = 600;
 
 bool initialize_window(void){
     
@@ -18,8 +24,8 @@ bool initialize_window(void){
     window = SDL_CreateWindow(NULL, 
         SDL_WINDOWPOS_CENTERED, 
         SDL_WINDOWPOS_CENTERED,
-        800,
-        600,
+        window_width,
+        window_height,
         SDL_WINDOW_BORDERLESS
     );
     
@@ -40,7 +46,14 @@ bool initialize_window(void){
 } 
 
 void setup(void){
-
+    color_buffer = (uint32_t*) malloc( sizeof(uint32_t) * window_width * window_height);
+    color_buffer_texture = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_ARGB8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        window_width,
+        window_height
+    );
 }
 
 void process_input(void){
@@ -63,7 +76,39 @@ void update(void){
 
 }
 
+void clear_color_buffer(uint32_t color){
+    for(int y= 0; y < window_height; y++ ){
+        for(int x= 0; x < window_width; x++ ){
+            color_buffer[(window_width * y) + x] = color;
+        }
+    }
+}
+
+void render_color_buffer(void){
+    SDL_UpdateTexture(
+        color_buffer_texture,
+        NULL,
+        color_buffer,
+        (int)(window_width * sizeof(uint32_t))
+    );
+    SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
+}
+
 void render(void){
+    SDL_SetRenderDrawColor(renderer, 255,0,0,255);
+    SDL_RenderClear(renderer);
+
+    render_color_buffer();
+    clear_color_buffer(0xFFFFFF00);
+
+    SDL_RenderPresent(renderer);
+}
+
+void destroy_all(void){
+    free(color_buffer);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
 }
 
@@ -80,5 +125,7 @@ int main(void){
 
     }
 
+    destroy_all();
+    
     return 0;
 }
