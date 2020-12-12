193bcb90a8523d4e80fec4b0cbb970a7f2c171e3
Chizco <franrulez@gmail.com>
2020-12-11 18:34:03 -0600

    buffer2

--- 3dRenderer/3drenderer/src/main.c
+++ 3dRenderer/3drenderer/src/main.c
@@ -21,6 +21,11 @@ bool initialize_window(void){
         return false;
     }
 
+    SDL_DisplayMode display_mode;
+    SDL_GetCurrentDisplayMode(0, &display_mode);
+    window_width = display_mode.w;
+    window_height = display_mode.h;
+        
     window = SDL_CreateWindow(NULL, 
         SDL_WINDOWPOS_CENTERED, 
         SDL_WINDOWPOS_CENTERED,
@@ -33,7 +38,7 @@ bool initialize_window(void){
         fprintf(stderr, "Window broken\n");
         return false;
     }
-
+    SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
     renderer = SDL_CreateRenderer(window, -1, 0);
 
     if(!renderer){
@@ -84,6 +89,22 @@ void clear_color_buffer(uint32_t color){
     }
 }
 
+void draw_grid(void){
+    for(int y= 0; y < window_height; y+=10 ){
+        for(int x= 0; x < window_width; x+=10 ){
+            color_buffer[(window_width * y) + x] = 0xff333333;   
+        }
+    }
+}
+
+void draw_rectange(int x,int y,int w, int h,uint32_t color){
+    for(int i = 0; i < w; i++ ){
+        for(int j= 0; j < h; j++ ){
+            color_buffer[(window_width * (y+j)) + (x+i)] = color;
+        }
+    }
+}
+
 void render_color_buffer(void){
     SDL_UpdateTexture(
         color_buffer_texture,
@@ -98,8 +119,10 @@ void render(void){
     SDL_SetRenderDrawColor(renderer, 255,0,0,255);
     SDL_RenderClear(renderer);
 
+    draw_grid();
+    draw_rectange(300,200,300,150,0xfff00fff);
     render_color_buffer();
-    clear_color_buffer(0xFFFFFF00);
+    clear_color_buffer(0x00000000);
 
     SDL_RenderPresent(renderer);
 }
